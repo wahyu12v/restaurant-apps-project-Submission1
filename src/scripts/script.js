7222ed8662f4bbd3b17dee7294de383a57e5bfec
wahyu12v <mkzcmd@gmail.com>
2024-03-31 23:59:30 +0700

    update v1 tinggal navbar

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,19 +1,33 @@
-const toggleDrawerButton = document.getElementById('toggleDrawer');
-const navigationDrawer = document.getElementById('navDrawer');
+// const toggleDrawerButton = document.getElementById('toggleDrawer');
+// const navigationDrawer = document.getElementById('navDrawer');
 
-toggleDrawerButton.addEventListener('click', () => {
-  navigationDrawer.classList.toggle('active');
-  toggleDrawerButton.querySelectorAll('div').forEach(div => {
-    div.classList.toggle('open');
-  });
-});
+// toggleDrawerButton.addEventListener('click', () => {
+//   navigationDrawer.classList.toggle('active');
+//   toggleDrawerButton.querySelectorAll('div').forEach(div => {
+//     div.classList.toggle('open');
+//   });
+// });
 
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 768) {
-    navigationDrawer.classList.remove('active');
-    toggleDrawerButton.querySelectorAll('div').forEach(div => {
-      div.classList.remove('open');
-    });
-  }
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("./data/DATA.json") 
+    .then(response => response.json())
+    .then(data => {
+      const restaurantsList = document.getElementById("restaurantsList");
+      data.restaurants.forEach(restaurant => {
+        const restaurantCard = `
+        
+          <div class="restaurant-card">
+            <img src="${restaurant.pictureId}" alt="${restaurant.name}">
+            <h2>${restaurant.name}</h2>
+            <p><strong>Kota:</strong> ${restaurant.city}</p>
+            <p><strong>Rating:</strong> ${restaurant.rating}</p>
+            <p>${restaurant.description}</p>
+          </div>
+        `;
+        restaurantsList.innerHTML += restaurantCard;
+      });
+    })
+    .catch(error => console.error("Error fetching data:", error));
 });
